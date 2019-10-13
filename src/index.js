a8914e68a5c05c9cf54ddc92f6a6aad60e581609
guenther <email@suepergue.de>
2019-10-13 13:58:47 +0200

    adds koa

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 const server = require("./server.js");
 
+server.init();
 console.log(server.sayHello());
--- src/server.js
+++ src/server.js
@@ -1,5 +1,16 @@
+const Koa = require("koa");
+
 const server = {
-  sayHello: () => "Hello World"
+  sayHello: () => "Hello World",
+  init: () => {
+    const app = new Koa();
+
+    app.use(async ctx => {
+      ctx.body = "Hello World";
+    });
+
+    app.listen(3000);
+  }
 };
 
 module.exports = server;
